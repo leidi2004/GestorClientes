760fe897bc5d96fa58fb01d7d54f35c117b99651
Leidi <leididelapuente3@gmail.com>
2024-02-26 23:35:18 -0500

    Agregar Sección pagina no encontrada

--- client/src/pages/NotFound/NotFound.jsx
+++ client/src/pages/NotFound/NotFound.jsx
@@ -1,5 +1,12 @@
+import { Link } from 'react-router-dom';
+import imagen404  from '../../assets/img/NotFoundImg.jpg';
+import styles from './NotFound.module.css'
 export const NotFound = () => {
-  return (
-    <div>NotFound</div>
-  )
-}
+	return (
+		<main className={styles.main}>
+			<h1 className={styles.main__title}>Pagina No Encontrada</h1>
+			<img className={styles.main__img} src={imagen404} alt='imagen de pagina no encontrada' />
+			<Link className={styles.main__link} to='/'>Volver al inicio</Link>
+		</main>
+	);
+};
